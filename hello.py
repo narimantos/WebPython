0ccbd324b418cd93fb6274e408a12aaf3f2aa50a
narimantos <narimantos@hotmail.com>
2017-09-12 22:50:23 +0200

    1e

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import cgitb
-cgitb.enable()
+cgitb.enable(display=0, logdir="/home/pi/logs/")
 print "Content-type: text/html\n\n"
 print "<h1>Hello World</h1>"
 print "<h2>Bye W0rld</h2>"
